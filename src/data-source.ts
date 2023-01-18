62c96ac9ce1446f131fba45eebeaa937f3a3bfc6
Gabriel Bessi <tecbessi@gmail.com>
2023-01-17 22:04:26 -0300

    fix: Refazendo Deploy

--- src/data-source.ts
+++ src/data-source.ts
@@ -4,11 +4,11 @@ import path from "path";
 import { DataSource, DataSourceOptions } from "typeorm";
 
 const setDataSourceConfig = (): DataSourceOptions => {
-  const entitiesPath: string = path.join(__dirname, "./entities/**.{js,ts}");
-  const migrationsPath: string = path.join(
-    __dirname,
-    "./migrations/**.{js,ts}"
-  );
+  // const entitiesPath: string = path.join(__dirname, "./entities/**.{js,ts}");
+  // const migrationsPath: string = path.join(
+  //   __dirname,
+  //   "./migrations/**.{js,ts}"
+  // );
 
   const nodeEnv = process.env.NODE_ENV;
 
@@ -16,17 +16,8 @@ const setDataSourceConfig = (): DataSourceOptions => {
     return {
       type: "postgres",
       url: process.env.DATABASE_URL,
-      entities: [entitiesPath],
-      migrations: [migrationsPath],
-    };
-  }
-
-  if (nodeEnv === "test") {
-    return {
-      type: "sqlite",
-      database: ":memory:",
-      synchronize: true,
-      entities: [entitiesPath],
+      entities: [path.join(__dirname, "./entities/**.{js,ts}")],
+      migrations: [path.join(__dirname, "./migrations/**.{js,ts}")],
     };
   }
 
